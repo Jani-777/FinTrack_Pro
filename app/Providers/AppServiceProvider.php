a7b0ca246be75f7b47dfafac9fd5e7b6590dc4c2
Jani-777 <johnlloydramos58@gmail.com>
2026-05-06 20:40:02 +0800

    Added dockerfile and updated APP_ENV

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Blade; // Import this!
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,6 +15,9 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
         // We create a custom Blade directive called @money
         Blade::directive('money', function ($expression) {
             return "<?php 
